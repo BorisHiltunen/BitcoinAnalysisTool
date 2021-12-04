af980412b1a1ad8e2efce20f2f2ebf216423419b
BorisHiltunen <90689146+BorisHiltunen@users.noreply.github.com>
2021-12-04 02:20:06 +0200

    Add changes

--- 2 API/UnRefactored/App.py
+++ 2 API/UnRefactored/App.py
@@ -73,6 +73,7 @@
 #Venv is needed for the module to work
 
 #How to get the data:
+from flask import Flask
 from datetime import datetime
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
@@ -91,6 +92,29 @@ cg = CoinGeckoAPI()
 #day = 86400
 #week = 604800
 #month = 2629743
+
+#IMPORTANT
+#CHECK
+#--------------------------------------------------------------------------------
+app2 = Flask(__name__)
+
+@app2.route('/')
+def main():
+    return "working"
+@app2.route('/get/DownTrend/')
+def getDownTrend():
+    return Application.getDownwardTrend1("25-11-2021", "30-11-2021")
+
+@app2.route('/get/HighestTradingVolume/')
+def getHighestTradingVolume():
+    return Application.getDownwardTrend1("25-11-2021", "30-11-2021")
+
+@app2.route('/get/BuyAndSellDates/')
+def getBuyAndSellDates():
+   # return Application.getHighestTradingVolume1("25-11-2021", "30-11-2021")
+   return "why"
+
+#--------------------------------------------------------------------------------
 
 class Application:
     def __init__(self):
@@ -167,6 +191,22 @@ class Application:
         timestamp = datetime.strptime(date_in_correct_form, "%d/%m/%Y").timestamp()
         return timestamp
 
+    #"25-11-2021|30-11-2021"
+    def getDates(self, date: str):
+        year1 = f"{date[6]}{date[7]}{date[8]}{date[9]}"
+        month1 = f"{date[3]}{date[4]}"
+        day1 = f"{date[0]}{date[1]}"
+
+        year2 = f"{date[17]}{date[18]}{date[19]}{date[20]}"
+        month2 = f"{date[14]}{date[15]}"
+        day2 = f"{date[11]}{date[12]}"
+
+        date_in_correct_form1 = f"{day1}/{month1}/{year1}"
+        date_in_correct_form2 = f"{day2}/{month2}/{year2}"
+        timestamp1 = datetime.strptime(date_in_correct_form1, "%d/%m/%Y").timestamp()
+        timestamp2 = datetime.strptime(date_in_correct_form2, "%d/%m/%Y").timestamp()
+        return timestamp1, timestamp2
+
     def convertTimestampToDate(self, timestamp):
         date = datetime.fromtimestamp(timestamp)
         return date
@@ -175,7 +215,7 @@ class Application:
         pass
 
     #Downward trend
-    def getDownwardTrend1(self, start: str, finish: str):
+    def getDownwardTrend1(self, dates: str):
 
         all_prices = []
         chosen_prices = []
@@ -183,8 +223,8 @@ class Application:
         count = 0
         most = 0
 
-        date1 = self.correctFormForCrypto2(start)
-        date2 = self.correctFormForCrypto2(finish)
+        date1 = self.getDates(dates)[0]
+        date2 = self.getDates(dates)[1]
         data = cg.get_coin_market_chart_range_by_id(id='bitcoin', vs_currency='eur', from_timestamp=date1, to_timestamp=date2)
 
         for price in data["prices"]:
@@ -212,62 +252,11 @@ class Application:
                 most = quantity
         return most
 
-    #Downward trend
-    def getDownwardTrend2(self, year1, month1, day1, year2, month2, day2):
-
-        all_prices = []
-        chosen_prices = []
-        chosen_price_quantities = []
-        count = 0
-        most = 0
-
-        date1 = self.correctFormForCrypto1(day1, month1, year1)
-        date2 = self.correctFormForCrypto1(day2, month2, year2)
-        data = cg.get_coin_market_chart_range_by_id(id='bitcoin', vs_currency='eur', from_timestamp=date1, to_timestamp=date2)
-
-        for price in data["prices"]:
-            all_prices.append(price[1])
-        
-        while count < len(all_prices):
-            if count == len(all_prices)-1:
-                if all_prices[count-1] > all_prices[count]:
-                    chosen_prices.append(all_prices[count])
-                else:
-                    chosen_price_quantities.append(len(chosen_prices))
-                    chosen_prices = []
-            else:
-                if all_prices[count] > all_prices[count+1]:
-                    if all_prices[count] not in chosen_prices:
-                        chosen_prices.append(all_prices[count])
-                    chosen_prices.append(all_prices[count+1])
-                else:
-                    chosen_price_quantities.append(len(chosen_prices))
-                    chosen_prices = []
-            count += 1
-
-        for quantity in chosen_price_quantities:
-            if quantity > most:
-                most = quantity
-        return most
-
     #HighestTradingVolume
-    def getHighestTradingVolume1(self, start: str, finish: str):
+    def getHighestTradingVolume1(self, dates: str):
 
-        date1 = self.correctFormForCrypto2(start)
-        date2 = self.correctFormForCrypto2(finish)
-        data = cg.get_coin_market_chart_range_by_id(id='bitcoin', vs_currency='eur', from_timestamp=date1, to_timestamp=date2)
-        highest = 0
-
-        for volume in data["total_volumes"]:
-            if volume[1] > highest:
-                highest = volume[1]
-        return highest
-
-    #HighestTradingVolume
-    def getHighestTradingVolume2(self, year1, month1, day1, year2, month2, day2):
-
-        date1 = self.correctFormForCrypto1(day1, month1, year1)
-        date2 = self.correctFormForCrypto1(day2, month2, year2)
+        date1 = self.getDates(dates)[0]
+        date2 = self.getDates(dates)[1]
         data = cg.get_coin_market_chart_range_by_id(id='bitcoin', vs_currency='eur', from_timestamp=date1, to_timestamp=date2)
         highest = 0
 
@@ -278,64 +267,14 @@ class Application:
 
     #TimeMachine
     #if more than 6 days +1 hour
-    def whenToBuyAndSell1(self, start: str, finish:str):
+    def whenToBuyAndSell1(self, dates: str):
 
         all_prices = []
         chosen_prices = []
         count = 0
 
-        date1 = self.correctFormForCrypto2(start)
-        date2 = self.correctFormForCrypto2(finish)
-        now = datetime.now()
-
-        if date2 != now.strftime("%d-%b-%Y (%H:%M:%S.%f)"):
-            date2 += 3600
-
-        data = cg.get_coin_market_chart_range_by_id(id='bitcoin', vs_currency='eur', from_timestamp=date1, to_timestamp=date2)
-
-        for price in data["prices"]:
-            all_prices.append(price[1])
-
-        lowest = "buy", date1, 1000000000
-
-        while count < len(all_prices):
-            if all_prices[count] < lowest[2]:
-                lowest = "buy", date1, all_prices[count]
-            date1 += 3600
-            count += 1
-
-        data2 = cg.get_coin_market_chart_range_by_id(id='bitcoin', vs_currency='eur', from_timestamp=lowest[1], to_timestamp=date2)
-        
-        for price in data2["prices"]:
-            chosen_prices.append(price[1])
-        
-        count = 0
-        date1 = lowest[1]
-        highest = "sell", date1, 0
-
-        while count < len(chosen_prices):
-            if chosen_prices[count] > highest[2]:
-                highest = "sell", date1, chosen_prices[count]
-            date1 += 3600
-            count += 1
-
-        if highest[2] < lowest[2]:
-            return "Don't buy"
-        else:
-            answer = (lowest[0], self.convertTimestampToDate(lowest[1]), lowest[2], highest[0], self.convertTimestampToDate(highest[1]), highest[2])
-
-            return answer
-
-    #TimeMachine
-    #if more than 6 days +1 hour
-    def whenToBuyAndSell2(self, year1, month1, day1, year2, month2, day2):
-
-        all_prices = []
-        chosen_prices = []
-        count = 0
-
-        date1 = self.correctFormForCrypto1(day1, month1, year1)
-        date2 = self.correctFormForCrypto1(day2, month2, year2)
+        date1 = self.getDates(dates)[0]
+        date2 = self.getDates(dates)[1]
         now = datetime.now()
 
         if date2 != now.strftime("%d-%b-%Y (%H:%M:%S.%f)"):
@@ -379,7 +318,7 @@ class Application:
     #TimeMachine
     #remember to add 1 hour to the to_timestamp
     #Think how to make this more simple
-    def whenToBuyAndSell3(self, start: str, finish:str):
+    def whenToBuyAndSell3(self, dates: str):
 
         chosen_prices = []
         count = 0
@@ -418,6 +357,7 @@ class Application:
 
 if __name__ == "__main__":
     app = Application()
+    app2.run()
 
     #Funktions used in the app
     #----------------------------------------------------
@@ -459,9 +399,9 @@ if __name__ == "__main__":
     #Buy ans sell test
     #print(app.whenToBuyAndSell1("25-11-2021", "30-11-2021"))
     #print(app.whenToBuyAndSell2(2021, 11, 25, 2021, 11, 30))
-    print(app.whenToBuyAndSell3("25-11-2021", "30-11-2021"))
+    print(app.whenToBuyAndSell3("25-11-2021|30-11-2021"))
 
     #Don't buy test
     #print(app.whenToBuyAndSell1("12-06-2021", "20-05-2021"))
     #print(app.whenToBuyAndSell2(2021, 6, 12, 2021, 5, 20))
-    print(app.whenToBuyAndSell3("12-06-2021", "20-05-2021"))
+    print(app.whenToBuyAndSell3("12-06-2021|20-05-2021"))
--- 2 API/UnRefactored/flask_test.py
+++ 2 API/UnRefactored/flask_test.py
@@ -1,19 +1,49 @@
 from flask import Flask
 
-app = Flask(__name__)
+from App import Application
 
-@app.route('/')
-def main(self):
-    pass
+app2 = Flask(__name__)
 
-@app.route('/')
-def getDownTrend(user_id):
-    pass
+@app2.route('/')
+def main():
+    return "working"
+    #return redirect(url_for('/get/getDownTrend/<dates>'))
+    #return redirect(url_for('/get/HighestTradingVolume/<dates>'))
+    #return redirect(url_for('/get/BuyAndSellDates/<dates>'))
 
-@app.route('/')
-def getHighestTradingVolume(user_id):
-    pass
+"""@app2.route('/get/getDownTrend/<dates>/')
+def getDownTrend(dates):
+    return Application.getDownwardTrend1(dates)
 
-@app.route('/user/<int:user_id>/')
-def getBuyAndSellDates(user_id):
-    pass
+@app2.route('/get/HighestTradingVolume/<dates>/')
+def getHighestTradingVolume(dates):
+    return Application.getDownwardTrend1(dates)
+
+@app2.route('/get/BuyAndSellDates/<dates>/')
+def getBuyAndSellDates(dates):
+    #print(start,finish)
+    #print(finish)
+    return Application.getHighestTradingVolume1(dates) 
+    #return  '{} {} {}'.format(Application.getHighestTradingVolume1(start, finish))
+    #return '{} {}'.format(start, finish)
+    #print('{}, {}'.format(start, finish))
+    #return Application.getHighestTradingVolume1('{}, {}'.format(start, finish))  """
+
+#start: str, finish:str
+#"12-06-2021", "20-05-2021"
+
+@app2.route('/get/DownTrend/')
+def getDownTrend():
+    return Application.getDownwardTrend1("25-11-2021|30-11-2021")
+
+@app2.route('/get/HighestTradingVolume/')
+def getHighestTradingVolume():
+    return Application.getDownwardTrend1("25-11-2021|30-11-2021")
+
+@app2.route('/get/BuyAndSellDates/')
+def getBuyAndSellDates():
+    print(Application.getHighestTradingVolume1("25-11-2021|30-11-2021") )
+    return Application.getHighestTradingVolume1("25-11-2021|30-11-2021") 
+
+if __name__ == "__main__":
+    app2.run()

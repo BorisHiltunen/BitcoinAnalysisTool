2694959db5029e659c6d8f3490bcd71bc8bca5d3
BorisHiltunen <90689146+BorisHiltunen@users.noreply.github.com>
2021-12-05 01:30:21 +0200

    Add changes

--- 2 API/UnRefactored/flask_test.py
+++ 2 API/UnRefactored/flask_test.py
@@ -3,6 +3,7 @@ from flask import Flask
 from App import Application
 
 app2 = Flask(__name__)
+app = Application()
 
 @app2.route('/')
 def main():
@@ -11,39 +12,36 @@ def main():
     #return redirect(url_for('/get/HighestTradingVolume/<dates>'))
     #return redirect(url_for('/get/BuyAndSellDates/<dates>'))
 
-"""@app2.route('/get/getDownTrend/<dates>/')
-def getDownTrend(dates):
-    return Application.getDownwardTrend1(dates)
+@app2.route('/get/')
+def getOptions():
+    return "1. DownTrend 2. HighestTradingVolume 3. BuyAndSellDates"
 
-@app2.route('/get/HighestTradingVolume/<dates>/')
-def getHighestTradingVolume(dates):
-    return Application.getDownwardTrend1(dates)
-
-@app2.route('/get/BuyAndSellDates/<dates>/')
-def getBuyAndSellDates(dates):
-    #print(start,finish)
-    #print(finish)
-    return Application.getHighestTradingVolume1(dates) 
-    #return  '{} {} {}'.format(Application.getHighestTradingVolume1(start, finish))
-    #return '{} {}'.format(start, finish)
-    #print('{}, {}'.format(start, finish))
-    #return Application.getHighestTradingVolume1('{}, {}'.format(start, finish))  """
-
-#start: str, finish:str
-#"12-06-2021", "20-05-2021"
-
+#Get data by writing dates in the code
 @app2.route('/get/DownTrend/')
 def getDownTrend():
-    return Application.getDownwardTrend1("25-11-2021|30-11-2021")
+    return str(app.getDownwardTrend1("25-11-2021|30-11-2021"))
 
 @app2.route('/get/HighestTradingVolume/')
 def getHighestTradingVolume():
-    return Application.getDownwardTrend1("25-11-2021|30-11-2021")
+    return str(app.getHighestTradingVolume1("25-11-2021|30-11-2021"))
 
 @app2.route('/get/BuyAndSellDates/')
 def getBuyAndSellDates():
-    print(Application.getHighestTradingVolume1("25-11-2021|30-11-2021") )
-    return Application.getHighestTradingVolume1("25-11-2021|30-11-2021") 
+    print(app.whenToBuyAndSell1("25-11-2021|30-11-2021"))
+    return str(app.whenToBuyAndSell1("25-11-2021|30-11-2021"))
+
+#Get data by writing dates in the url
+@app2.route('/get/DownTrend/<dates>/')
+def getDownTrendWithUrl(dates):
+    return str(app.getDownwardTrend1(dates))
+
+@app2.route('/get/HighestTradingVolume/<dates>/')
+def getHighestTradingVolumeWithUrl(dates):
+    return str(app.getDownwardTrend1(dates))
+
+@app2.route('/get/BuyAndSellDates/<string:dates>/')
+def getBuyAndSellDatesWithUrl(dates):
+    return str(app.whenToBuyAndSell1(dates))
 
 if __name__ == "__main__":
     app2.run()

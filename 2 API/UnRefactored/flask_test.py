3d836497037877cd0d52580982a1178ee535fede
BorisHiltunen <90689146+BorisHiltunen@users.noreply.github.com>
2021-12-06 20:56:16 +0200

    Add changes

--- 2 API/UnRefactored/flask_test.py
+++ 2 API/UnRefactored/flask_test.py
@@ -18,29 +18,28 @@ def getOptions():
 #Get data by writing dates in the code
 @app2.route('/get/DownTrend/')
 def getDownTrend():
-    return str(app.getDownwardTrend1("25-11-2021|30-11-2021"))
+    return str(app.getDownwardTrend("25-11-2021|30-11-2021"))
 
 @app2.route('/get/HighestTradingVolume/')
 def getHighestTradingVolume():
-    return str(app.getHighestTradingVolume1("25-11-2021|30-11-2021"))
+    return str(app.getHighestTradingVolume("25-11-2021|30-11-2021"))
 
 @app2.route('/get/BuyAndSellDates/')
 def getBuyAndSellDates():
-    print(app.whenToBuyAndSell1("25-11-2021|30-11-2021"))
-    return str(app.whenToBuyAndSell1("25-11-2021|30-11-2021"))
+    return str(app.whenToBuyAndSell("25-11-2021|30-11-2021"))
 
 #Get data by writing dates in the url
 @app2.route('/get/DownTrend/<dates>/')
 def getDownTrendWithUrl(dates):
-    return str(app.getDownwardTrend1(dates))
+    return str(app.getDownwardTrend(dates))
 
 @app2.route('/get/HighestTradingVolume/<dates>/')
 def getHighestTradingVolumeWithUrl(dates):
-    return str(app.getDownwardTrend1(dates))
+    return str(app.getDownwardTrend(dates))
 
 @app2.route('/get/BuyAndSellDates/<string:dates>/')
 def getBuyAndSellDatesWithUrl(dates):
-    return str(app.whenToBuyAndSell1(dates))
+    return str(app.whenToBuyAndSell(dates))
 
 if __name__ == "__main__":
     app2.run()

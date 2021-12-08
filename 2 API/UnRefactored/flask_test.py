4fc502ea186e915ba6a1fde3925e38c9cdb4d0aa
BorisHiltunen <90689146+BorisHiltunen@users.noreply.github.com>
2021-12-08 15:00:31 +0200

    Add changes

--- 2 API/UnRefactored/flask_test.py
+++ 2 API/UnRefactored/flask_test.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 from App import Application
 
 app2 = Flask(__name__)
@@ -7,39 +7,39 @@ app = Application()
 @app2.route('/')
 def main():
     return "working"
-    #return redirect(url_for('/get/getDownTrend/<dates>'))
-    #return redirect(url_for('/get/HighestTradingVolume/<dates>'))
-    #return redirect(url_for('/get/BuyAndSellDates/<dates>'))
+    #return redirect(url_for('/get/downward_trend/<dates>'))
+    #return redirect(url_for('/get/highest_trading_volume/<dates>'))
+    #return redirect(url_for('/get/buy_and_sell_dates/<dates>'))
 
 @app2.route('/get/')
-def getOptions():
-    return "1. DownTrend 2. HighestTradingVolume 3. BuyAndSellDates"
+def get_options():
+    return "1. downward_trend 2. highest_trading_volume 3. buy_and_sell_dates"
 
 #Get data by writing dates in the code
-@app2.route('/get/DownTrend/')
-def getDownTrend():
-    return str(app.getDownwardTrend("25-11-2021|30-11-2021"))
+@app2.route('/get/downward_trend/')
+def get_downward_trend():
+    return str(app.get_downward_trend("25-11-2021|30-11-2021"))
 
-@app2.route('/get/HighestTradingVolume/')
-def getHighestTradingVolume():
-    return str(app.getHighestTradingVolume("25-11-2021|30-11-2021"))
+@app2.route('/get/highest_trading_volume/')
+def get_highest_trading_volume():
+    return str(app.get_highest_trading_volume("25-11-2021|30-11-2021"))
 
-@app2.route('/get/BuyAndSellDates/')
-def getBuyAndSellDates():
-    return str(app.getBuyAndSellDates("25-11-2021|30-11-2021"))
+@app2.route('/get/buy_and_sell_dates/')
+def get_buy_and_sell_dates_with_url():
+    return str(app.get_buy_and_sell_dates("25-11-2021|30-11-2021"))
 
 #Get data by writing dates in the url
-@app2.route('/get/DownTrend/<dates>/')
-def getDownTrendWithUrl(dates):
-    return str(app.getDownwardTrend(dates))
+@app2.route('/get/downward_trend/<dates>/')
+def get_downward_trend_with_url(dates):
+    return str(app.get_downward_trend(dates))
 
-@app2.route('/get/HighestTradingVolume/<dates>/')
-def getHighestTradingVolumeWithUrl(dates):
-    return str(app.getDownwardTrend(dates))
+@app2.route('/get/highest_trading_volume/<dates>/')
+def get_highest_trading_volume_with_url(dates):
+    return str(app.get_highest_trading_volume(dates))
 
-@app2.route('/get/BuyAndSellDates/<string:dates>/')
-def getBuyAndSellDatesWithUrl(dates):
-    return str(app.getBuyAndSellDates(dates))
+@app2.route('/get/buy_and_sell_dates/<string:dates>/')
+def get_buy_and_sell_dates_with_url(dates):
+    return str(app.get_buy_and_sell_dates(dates))
 
 if __name__ == "__main__":
     app2.run()

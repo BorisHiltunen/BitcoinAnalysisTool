c72b6447a6d2d39ab4d9cbddec777cdc0f7eaaef
BorisHiltunen <90689146+BorisHiltunen@users.noreply.github.com>
2021-12-15 14:59:05 +0200

    Noticed JSON format problem and started working on it

--- 2 API/UnRefactored/routes.py
+++ 2 API/UnRefactored/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from main import Application
 
 app = Flask(__name__)
@@ -18,17 +18,32 @@ def get_options():
     return "1. downward_trend 2. highest_trading_volume 3. buy_and_sell_dates"
 
 #Get data by writing dates in the url
+
+# Not in the right form
 @app.route('/get/downward_trend/<dates>/')
 def get_downward_trend_with_url(dates):
-    return str(application.get_downward_trend(dates))
+    return (application.get_downward_trend(dates)), 200
 
 @app.route('/get/highest_trading_volume/<dates>/')
 def get_highest_trading_volume_with_url(dates):
-    return str(application.get_highest_trading_volume(dates))
+    return application.get_highest_trading_volume(dates), 200
 
 @app.route('/get/buy_and_sell_dates/<string:dates>/')
 def get_buy_and_sell_dates_with_url(dates):
-    return str(application.get_buy_and_sell_dates(dates))
+    return application.get_buy_and_sell_dates(dates), 200
+
+# Testing
+"""@app.route('/get/downward_trend/<dates>/')
+def get_downward_trend_with_url(dates):
+    return jsonify(str(application.get_downward_trend(dates))), 200
+
+@app.route('/get/highest_trading_volume/<dates>/')
+def get_highest_trading_volume_with_url(dates):
+    return jsonify(str(application.get_highest_trading_volume(dates))), 200
+
+@app.route('/get/buy_and_sell_dates/<string:dates>/')
+def get_buy_and_sell_dates_with_url(dates):
+    return jsonify(str(application.get_buy_and_sell_dates(dates))), 200"""
 
 if __name__ == "__main__":
     app.run()
